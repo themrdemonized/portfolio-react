fef686eae3ac6b44a35cff22a271251a77bee43b
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2023-08-26 22:22:07 +1000

    init

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// jest.config.js
+module.exports = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    moduleNameMapper: {
+        '\\.(scss|sass|css)$': 'identity-obj-proxy',
+      },
+    transform: {
+        "^.+\\.(t|j)sx?$": "@swc/jest",
+    },
+  };
+  
