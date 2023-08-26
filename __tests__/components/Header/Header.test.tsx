56f164e616f43b805f20ecba57b1fdc96d3e5c66
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2023-08-27 01:09:40 +1000

    cypress

--- __tests__/components/Header/Header.test.tsx
+++ __tests__/components/Header/Header.test.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/Header/Header";
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Header', () => {
-    it('renders a heading', () => {
-      const { container } = render(<Header />);
-   
-      expect(container).toBeInTheDocument();
-    })
-  })
+	it('renders a heading', () => {
+		const { container } = render(<Header />);
+
+		expect(container).toBeInTheDocument();
+	})
+})
