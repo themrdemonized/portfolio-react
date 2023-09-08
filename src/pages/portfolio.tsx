8ddae7ab1220112216e6ae56db50da44c8725245
DEMONIZED-PC\demonized <themrdemonized@gmail.com>
2023-09-09 01:13:45 +1000

    portfolio pic

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,7 +1,7 @@
 import Animatable from "@/components/Animatable"
 import PortfolioCard from "@/components/PortfolioCard/PortfolioCard"
 import { useDict } from "@/dictionary/DictionaryContext"
-import { Typography } from "@mui/joy"
+import { SvgIcon, Typography } from "@mui/joy"
 
 import Image from "next/image"
 import YARLImageSlide from "@/components/YARLImageSlide/YARLImageSlide"
@@ -12,9 +12,33 @@ import { Thumbnails, Inline } from "yet-another-react-lightbox/plugins"
 import portfolioPic from "@/../public/assets/portfolio_preview.jpg"
 import rescriptPic1 from "@/../public/assets/rescript_preview.jpg"
 import rescriptPic2 from "@/../public/assets/rescript_preview2.jpg"
+import rescriptPic3 from "@/../public/assets/rescript_preview3.jpg"
+import dviprazPic1 from "@/../public/assets/dvipraz1.jpg"
+import dviprazPic2 from "@/../public/assets/dvipraz2.jpg"
+import dviprazLkPic1 from "@/../public/assets/dviprazlk1.jpg"
+import dviprazLkPic2 from "@/../public/assets/dviprazlk2.jpg"
+import dviprazLkPic3 from "@/../public/assets/dviprazlk3.jpg"
+import dviprazLkPic4 from "@/../public/assets/dviprazlk4.jpg"
+import dviprazLkPic5 from "@/../public/assets/dviprazlk5.jpg"
+import uipPic1 from "@/../public/assets/uip1.jpg"
+import uipPic2 from "@/../public/assets/uip2.jpg"
+import uipPic3 from "@/../public/assets/uip3.jpg"
+import lstarPic1 from "@/../public/assets/lstar1.jpg"
+import lstarPic2 from "@/../public/assets/lstar2.jpg"
+import lstarPic3 from "@/../public/assets/lstar3.jpg"
+import vssPic1 from "@/../public/assets/vss1.jpg"
+import vssPic2 from "@/../public/assets/vss2.jpg"
+import vssPic3 from "@/../public/assets/vss3.jpg"
+import asiazipPic1 from "@/../public/assets/asiazip1.jpg"
+import asiazipPic2 from "@/../public/assets/asiazip2.jpg"
+import asiazipPic3 from "@/../public/assets/asiazip3.jpg"
 
 import { useState } from "react"
 import YARLCarousel from "@/components/YARLCarousel/YARLCarousel"
+import MyLink from "@/components/Link/MyLink"
+
+import Github from "@mui/icons-material/GitHub"
+import Link from "@mui/icons-material/Link"
 
 export default function Portfolio() {
 	const {dict} = useDict()
@@ -30,55 +54,155 @@ export default function Portfolio() {
 					stack={dict.portfolio.portfolio_site.stack}
 					date={dict.portfolio.portfolio_site.date}
 					images={[portfolioPic]}
+					links={
+						<>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="fdfs">
+									<Github />
+									<span>Github</span>
+								</MyLink>
+							</p>
+						</>
+					}
 				/>
 				<PortfolioCard
 					title={dict.portfolio.rescript_elements.title}
 					description={dict.portfolio.rescript_elements.description}
 					stack={dict.portfolio.rescript_elements.stack}
 					date={dict.portfolio.rescript_elements.date}
-					images={[rescriptPic1, rescriptPic2]}
+					images={[rescriptPic1, rescriptPic2, rescriptPic3]}
+					links={
+						<>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://react-nextjs-rescript-elements.vercel.app/">
+									<Link />
+									<span>Demo</span>
+								</MyLink>
+							</p>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://github.com/themrdemonized/react-nextjs-rescript-elements">
+									<Github />
+									<span>Github</span>
+								</MyLink>
+							</p>
+						</>
+					}
 				/>
 				<PortfolioCard
 					title={dict.portfolio.dvipraz.title}
 					description={dict.portfolio.dvipraz.description}
 					stack={dict.portfolio.dvipraz.stack}
 					date={dict.portfolio.dvipraz.date}
-					images={[portfolioPic]}
+					images={[dviprazPic1, dviprazPic2]}
+					links={
+						<>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://dvipraz.ru/">
+									<Link />
+									<span>Demo</span>
+								</MyLink>
+							</p>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://github.com/themrdemonized/dvipras">
+									<Github />
+									<span>Github</span>
+								</MyLink>
+							</p>
+						</>
+					}
 				/>
 				<PortfolioCard
 					title={dict.portfolio.dvipraz_lk.title}
 					description={dict.portfolio.dvipraz_lk.description}
 					stack={dict.portfolio.dvipraz_lk.stack}
 					date={dict.portfolio.dvipraz_lk.date}
-					images={[portfolioPic]}
+					images={[
+						dviprazLkPic1,
+						dviprazLkPic2,
+						dviprazLkPic3,
+						dviprazLkPic4,
+						dviprazLkPic5
+					]}
 				/>
 				<PortfolioCard
 					title={dict.portfolio.uipdv.title}
 					description={dict.portfolio.uipdv.description}
 					stack={dict.portfolio.uipdv.stack}
 					date={dict.portfolio.uipdv.date}
-					images={[portfolioPic]}
+					images={[uipPic1, uipPic2, uipPic3]}
+					links={
+						<>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://uipdv.ru">
+									<Link />
+									<span>Demo</span>
+								</MyLink>
+							</p>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://github.com/themrdemonized/UIP-Laravel">
+									<Github />
+									<span>Github</span>
+								</MyLink>
+							</p>
+						</>
+					}
 				/>
 				<PortfolioCard
 					title={dict.portfolio.lstar.title}
 					description={dict.portfolio.lstar.description}
 					stack={dict.portfolio.lstar.stack}
 					date={dict.portfolio.lstar.date}
-					images={[portfolioPic]}
+					images={[lstarPic1, lstarPic2, lstarPic3]}
+					links={
+						<>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://l-star.ru">
+									<Link />
+									<span>Demo</span>
+								</MyLink>
+							</p>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://github.com/themrdemonized/Little-Star-Store-OpenCart">
+									<Github />
+									<span>Github</span>
+								</MyLink>
+							</p>
+						</>
+					}
 				/>
 				<PortfolioCard
 					title={dict.portfolio.vss.title}
 					description={dict.portfolio.vss.description}
 					stack={dict.portfolio.vss.stack}
 					date={dict.portfolio.vss.date}
-					images={[portfolioPic]}
+					images={[vssPic1, vssPic2, vssPic3]}
+					links={
+						<>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://ventstroysnab.ru/">
+									<Link />
+									<span>Demo</span>
+								</MyLink>
+							</p>
+						</>
+					}
 				/>
 				<PortfolioCard
 					title={dict.portfolio.asiazip.title}
 					description={dict.portfolio.asiazip.description}
 					stack={dict.portfolio.asiazip.stack}
 					date={dict.portfolio.asiazip.date}
-					images={[portfolioPic]}
+					images={[asiazipPic1, asiazipPic2, asiazipPic3]}
+					links={
+						<>
+							<p>
+								<MyLink target="_blank" rel="noopener noreferrer" href="https://asiazip.ru">
+									<Link />
+									<span>Demo</span>
+								</MyLink>
+							</p>
+						</>
+					}
 				/>
 			</div>
 		</Animatable>
