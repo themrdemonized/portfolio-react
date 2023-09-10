b47c25dba02e3a5fc08a944b636d15c1666048e5
KOMPUTER\Оля <themrdemonized@gmail.com>
2023-09-11 05:43:56 +1000

    profile with links pass

--- src/components/Link/MyLink.tsx
+++ src/components/Link/MyLink.tsx
@@ -2,11 +2,13 @@ import JoyLink, { LinkProps as JoyLinkProps } from "@mui/joy/Link"
 import clsx from "clsx"
 import styles from "./MyLink.module.scss"
 
-export default function MyLink({ children, ...rest }: JoyLinkProps) {
+export default function MyLink({ children, className, ...rest }: JoyLinkProps) {
+	const cl = className?.split(' ').reduce((acc, value) => { return {...acc, [value]: true} }, {})
 	return (
 		<JoyLink
 			{...rest}
 			className={clsx({
+				...cl,
 				[styles.link]: true
 			})}
 		>
--- src/components/ProfilePicWithLinks/ProfilePicWithLinks.tsx
+++ src/components/ProfilePicWithLinks/ProfilePicWithLinks.tsx
@@ -4,19 +4,18 @@ import Image from "next/image"
 import LinkedIn from '@mui/icons-material/LinkedIn'
 import Github from '@mui/icons-material/GitHub'
 import MyLink from '../Link/MyLink'
+import styles from './ProfilePicWithLinks.module.scss'
 
 export default function ProfilePicWithLinks() {
     const {dict} = useDict()
     return (
         <div className='flex items-center gap-2 max-w-sm'>
-            <div className=''>
+            <div>
                 <MyLink 
                     href='https://www.linkedin.com/in/dmitry-chernyavsky-a2322b284/'
                     target='_blank'
                     rel='noopener noreferrer'
-                    style={{
-                        border: 'none!important'
-                    }}
+                    className={styles.profilePicLink}
                 >
                     <span className='block rounded-full overflow-hidden'>
                         <Image
